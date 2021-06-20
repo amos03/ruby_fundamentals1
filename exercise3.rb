c9bdea1e6b34d899ce606b7dffbb0e2823d8a02e
amos03 <amossi@gmail.com>
2021-06-20 16:41:26 -0400

    age - year calculation but output will not be accurate without further work to determine if user has already celebrate a birthday during this calendar year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 user_name = gets.chomp
 puts "Hello, #{user_name}."
+puts "How old are you #{user_name}?"
+user_age = gets.chomp.to_i
+puts "#{user_name}, you were born in the year #{2021-user_age}."
