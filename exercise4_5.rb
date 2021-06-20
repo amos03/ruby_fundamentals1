cdbe7cb51d892eb0d3d0bbfea02d0dccae54d1a1
amos03 <amossi@gmail.com>
2021-06-20 18:11:55 -0400

    exercise 4.5 some tweaking using absolute value

--- exercise4_5.rb
+++ exercise4_5.rb
@@ -1,9 +1,9 @@
 secret_number = 613
-puts "Please enter a whole number:"
+puts "Please enter a number:"
 user_number = gets.chomp.to_i
 if user_number == secret_number
     puts "You win!"
-elsif secret_number - user_number.abs == 1 || secret_number - user_number.abs == -1 
+elsif secret_number - user_number == 1 || secret_number - user_number == -1 
     puts "So close!"
 else
     puts "Try again!"
