5eac7f6d303c1c856edd683e2fcc2d289a70a552
amos03 <amossi@gmail.com>
2021-06-20 16:30:20 -0400

    output of question 4

--- exercise2.rb
+++ exercise2.rb
@@ -6,3 +6,5 @@ tip_amount = price_meal * tip_percentage
 puts "Your tip is $"+ tip_amount.to_s 
 
 puts "The number 45628 multiplied by 7839 equals #{45628 * 7839}."
+
+puts "The statement 10<20 && 30 < 20) || !(10==11) is #{(10<20 && 30 < 20) || !(10==11)}."
