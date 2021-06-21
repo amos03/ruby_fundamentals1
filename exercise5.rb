7288fa196e969c83bb7550e865ad82ade3817458
amos03 <amossi@gmail.com>
2021-06-20 21:44:26 -0400

    energy counter now working but refinement coudl be made for indicating when zero reached.

--- exercise5.rb
+++ exercise5.rb
@@ -2,7 +2,7 @@ total_distance = 0
 user_response = "empty"
 energy = 10
 
-until user_response == "go home"
+until user_response == "go home" || energy ==0
     puts "Your energy level is currently #{energy}."
     puts "Do you feel like walking or running (type w or r)? To exit write \"go home\"."
 
@@ -16,7 +16,7 @@ elsif user_response =="r"
     total_distance = total_distance + 5
     puts "Distance from home is #{total_distance}"
     energy = energy - 1
-elsif user_response == "go home" || energy == 0
+elsif user_response == "go home"
     else 
         puts "I did not recognize that command. Please try again."
 end
