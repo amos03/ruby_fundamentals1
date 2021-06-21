c346c83842ae387148603f51e363a72965af64bf
amos03 <amossi@gmail.com>
2021-06-20 21:57:06 -0400

    fixed energy counter indicator for when it reaches zero

--- exercise5.rb
+++ exercise5.rb
@@ -1,9 +1,10 @@
 total_distance = 0
 user_response = "empty"
 energy = 10
+puts "Your energy level is currently #{energy}."
 
 until user_response == "go home" || energy ==0
-    puts "Your energy level is currently #{energy}."
+    
     puts "Do you feel like walking or running (type w or r)? To exit write \"go home\"."
 
 user_response = gets.chomp
@@ -12,13 +13,15 @@ if user_response == "w"
     total_distance = total_distance + 1
     puts "Distance from home is #{total_distance}"
     energy = energy + 1
+    puts "Your energy level is now #{energy}."
 elsif user_response =="r"
     total_distance = total_distance + 5
     puts "Distance from home is #{total_distance}"
     energy = energy - 1
+    puts "Your energy level is now #{energy}."
 elsif user_response == "go home"
     else 
         puts "I did not recognize that command. Please try again."
-end
+end 
 
 end
