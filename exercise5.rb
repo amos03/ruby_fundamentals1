4db157766812226b7f40a7dffc21c8a448d63d60
amos03 <amossi@gmail.com>
2021-06-20 22:48:08 -0400

    added eating function to replenish energy 6.2

--- exercise5.rb
+++ exercise5.rb
@@ -1,11 +1,11 @@
 total_distance = 0
 user_response = "empty"
 energy = 10
-puts "Your energy level is currently #{energy}."
+puts "Your energy level is currently #{energy}. Make sure it does not decline to zero. Walking increases your energy by 1; running decreases it by 1."
 
 until user_response == "go home" || energy ==0
     
-    puts "Do you feel like walking or running (type w or r)? To exit write \"go home\"."
+    puts "Do you feel like walking or running (type w or r)? To eat, type e and replenish your energy by 1. To exit write \"go home\"."
 
 user_response = gets.chomp
 
@@ -18,6 +18,10 @@ elsif user_response =="r"
     total_distance = total_distance + 5
     puts "Distance from home is #{total_distance}"
     energy = energy - 1
+    puts "Your energy level is now #{energy}."
+elsif user_response=="e"
+    puts "Distance from home is #{total_distance}"
+    energy = energy + 1
     puts "Your energy level is now #{energy}."
 elsif user_response == "go home"
     else 
