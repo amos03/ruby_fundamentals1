2a904f9ae3bacd9540dff1b64a1cc9fb70798f81
amos03 <amossi@gmail.com>
2021-06-21 08:52:56 -0400

    added option to replenish energy when down to zero using nested if

--- exercise5.rb
+++ exercise5.rb
@@ -3,6 +3,9 @@ user_response = "empty"
 energy = 10
 puts "Your energy level is currently #{energy}. Make sure it does not decline to zero. Walking increases your energy by 1; running decreases it by 1."
 
+
+    
+
 until user_response == "go home" || energy ==0
     
     puts "Do you feel like walking or running (type w or r)? To eat, type e and replenish your energy by 1. To exit write \"go home\"."
@@ -19,6 +22,17 @@ elsif user_response =="r"
     puts "Distance from home is #{total_distance}"
     energy = energy - 1
     puts "Your energy level is now #{energy}."
+
+    if energy == 0
+        puts "You are out of energy. Would you like to increase energy by eating something? (y/n)"
+        outofenergy_prompt = gets.chomp
+        if outofenergy_prompt=="y" || outofenergy_prompt=="yes"
+            energy = energy + 1
+            puts "Your energy level has been restored to #{energy}."
+        end
+    end
+    
+
 elsif user_response=="e"
     puts "Distance from home is #{total_distance}"
     energy = energy + 1
